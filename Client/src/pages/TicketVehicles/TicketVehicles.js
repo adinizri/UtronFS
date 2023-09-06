b4eb8a4be3eadabdc8b71f4fecc5768f467674b2
Adi <adi@Adis-MacBook-Pro.local>
2023-09-06 08:47:57 +0300

    style

--- Client/src/pages/TicketVehicles/TicketVehicles.js
+++ Client/src/pages/TicketVehicles/TicketVehicles.js
@@ -2,46 +2,60 @@ import { Select } from "antd";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { AxiosHeaders, SERVER_ADRESS } from "../../consts";
-import "./TicketVehicles.css"
-const TicketVehicles=()=>{
-    const [ticketType,setTicketType]= useState("");
-    const [vehicles,setVehicles]=useState()
-    const ticketTypes=["VIP","Value","Regular"];
+import "./TicketVehicles.css";
 
-const createOptions=()=>{
-    const options=[];
-    ticketTypes.map(ticketType=>options.push({value:ticketType}));
-    console.log(options)
-    return options;
-}
-const onChange = (value) => {
-  console.log(`selected ${value}`);
-  setTicketType(value)
-};
+const TicketVehicles = () => {
+  const [ticketType, setTicketType] = useState("");
+  const [vehicles, setVehicles] = useState(null); // Initialize as null
+  const ticketTypes = ["VIP", "Value", "Regular"];
 
-useEffect(() => {
-   async function fetchVehicles(){
-   const resualt=await axios.get(`${SERVER_ADRESS}/api/Garage/ticketParkingCars/${ticketType}`,{headers:AxiosHeaders
-    })
-    setVehicles(resualt.data)
-   }
-   fetchVehicles()
-}, [ticketType]);
+  const createOptions = () => {
+    return ticketTypes.map((ticketType) => ({ value: ticketType }));
+  };
 
+  const onChange = (value) => {
+    console.log(`selected ${value}`);
+    setTicketType(value);
+  };
 
+  useEffect(() => {
+    async function fetchVehicles() {
+      if (ticketType) {
+        const result = await axios.get(`${SERVER_ADRESS}/api/Garage/ticketParkingCars/${ticketType}`, {
+          headers: AxiosHeaders,
+        });
+        setVehicles(result.data);
+      }
+    }
+    fetchVehicles();
+  }, [ticketType]);
 
-    return(
+  return (
     <div className="container">
       <h2>Show ticket vehicles</h2>
-      <Select
-        showSearch
-        placeholder="Select ticket type"
-        onChange={onChange}
-        options={createOptions()}
-        style={{ width: 200 }} // Adjust the width as needed
-      />
-      { vehicles&&<><div>vehicles:</div><div>{vehicles.map(vehicle=><p key={vehicle}>{vehicle}</p>)}</div></>}
-    </div>)
+      <div className="select-container">
+        <Select
+          showSearch
+          placeholder="Select ticket type"
+          onChange={onChange}
+          options={createOptions()}
+          style={{ width: 200 }} // Adjust the width as needed
+        />
+      </div>
+      <div className="vehicles-container">
+        {vehicles && (
+          <>
+            <div className="vehicles-label">Vehicles:</div>
+            <div className="vehicle-list">
+              {vehicles.map((vehicle) => (
+                <p key={vehicle}>{vehicle}</p>
+              ))}
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
 
-}
 export default TicketVehicles;
