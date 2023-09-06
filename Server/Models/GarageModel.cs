cc6433aa3961c2d949136e2947aa2767a3a44efd
Adi Nizri <adin624@gmail.com>
2023-09-06 20:14:29 +0300

    fixing insert async

--- Server/Models/GarageModel.cs
+++ Server/Models/GarageModel.cs
@@ -6,6 +6,7 @@ class GarageModel
 {
     private readonly UtronDbContext _context;
     private readonly Ticket _ticket;
+    private static readonly SemaphoreSlim parkingSpotLock = new(1, 1);
 
     public GarageModel(UtronDbContext context)
     {
@@ -14,32 +15,43 @@ class GarageModel
     }
 
     //checkin in a vehicle
+
     public async Task<ParkingRecord> CheckInVehicle(VehicleModel vehicle)
     {
         try
         {
-            // check if there free lots
-            List<int> ticketTypeFreeLots = await _ticket.GetFreeParkingLotByTicketType(
-                vehicle.TicketType
-            );
-            if (ticketTypeFreeLots.Count() > 0)
-            {
-                ParkingRecord parkingRecord =
-                    new()
-                    {
-                        LicensePlateID = vehicle.LicensePlateID,
-                        ParkingDateTime = DateTime.Now,
-                        LotNumber = ticketTypeFreeLots[0],
-                        PhoneNumber = vehicle.Phone,
-                        Name = vehicle.Name
-                    };
+            await parkingSpotLock.WaitAsync();
 
-                await _context.ParkingRecords.AddAsync(parkingRecord);
-                await _context.SaveChangesAsync();
-                return parkingRecord;
-            }
-            else
+            try
+            {
+                // Check if there are free lots
+                List<int> ticketTypeFreeLots = await _ticket.GetFreeParkingLotByTicketType(
+                    vehicle.TicketType
+                );
+
+                if (ticketTypeFreeLots.Count() > 0)
+                {
+                    ParkingRecord parkingRecord =
+                        new()
+                        {
+                            LicensePlateID = vehicle.LicensePlateID,
+                            ParkingDateTime = DateTime.Now,
+                            LotNumber = ticketTypeFreeLots[0],
+                            PhoneNumber = vehicle.Phone,
+                            Name = vehicle.Name
+                        };
+
+                    _context.ParkingRecords.Add(parkingRecord);
+                    await _context.SaveChangesAsync();
+                    return parkingRecord;
+                }
+
                 return null;
+            }
+            finally
+            {
+                parkingSpotLock.Release();
+            }
         }
         catch (Exception ex)
         {
